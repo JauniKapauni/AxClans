3c0d1575b6d720d48f0bf96b91211d633ffc5670
JauniKapauni <riches-goods.54@icloud.com>
2026-05-15 13:39:32 +0200

    feat: add DatabaseManager

--- src/main/java/de/jaunikapauni/axclans/AxClans.java
+++ src/main/java/de/jaunikapauni/axclans/AxClans.java
@@ -1,12 +1,28 @@
 package de.jaunikapauni.axclans;
 
+import de.jaunikapauni.axclans.manager.DatabaseManager;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class AxClans extends JavaPlugin {
+    DatabaseManager databaseManager;
+    public DatabaseManager getDatabaseManager(){
+        return databaseManager;
+    }
 
     @Override
     public void onEnable() {
         // Plugin startup logic
+        saveDefaultConfig();
+        try{
+            databaseManager = new DatabaseManager(this);
+            if(databaseManager.initDatabaseTable1() || databaseManager.initDatabaseTable2() == false){
+                getLogger().severe("Error creating table!");
+                Bukkit.getServer().shutdown();
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
